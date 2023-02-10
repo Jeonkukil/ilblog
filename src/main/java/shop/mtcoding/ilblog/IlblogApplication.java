2860426de728a28aa73ab35c5e6c95f92755d3d1
Jeonkukil <a0211a@naver.com>
2023-02-11 01:14:04 +0900

    8080연결 확인

---
+++ src/main/java/shop/mtcoding/ilblog/IlblogApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.ilblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/ilblog/IlblogApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.ilblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
