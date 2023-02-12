4d771e79eea0d78c7bb2514ef33db3c2a437f873
Jeonkukil <a0211a@naver.com>
2023-02-13 01:40:11 +0900

    detail완

--- src/main/java/shop/mtcoding/ilblog/model/BoardRepository.java
+++ src/main/java/shop/mtcoding/ilblog/model/BoardRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import shop.mtcoding.ilblog.dto.board.BoardResp.BoardDetailRespDto;
 import shop.mtcoding.ilblog.dto.board.BoardResp.BoardMainRespDto;
 
 @Mapper
@@ -28,4 +29,6 @@ public interface BoardRepository {
                         @Param("content") String content);
 
         public int deleteById(int id);
+
+        public BoardDetailRespDto findByIdWithUser(int id);
 }
